4f182d6c49379e469fcdabee3cc9cda3e1a3e504
hungpt <hungpt123zed@gmail.com>
2025-03-05 22:25:01 +0700

    Init

---
+++ Assets/Scripts/Core/PickItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItem : MonoBehaviour
+{
+    public void OnPointClick()
+    {
+        Debug.Log("ALo");
+    }
+
+}
